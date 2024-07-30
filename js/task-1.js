c386bf843d8b77d037610d91da6c78ee7f14cf6c
alyona <akovalcuk485@gmail.com>
2024-07-30 18:23:23 +0300

    finish markup

--- js/task-1.js
+++ js/task-1.js
@@ -1,16 +1,54 @@
 'use strict';
 
-function isEnoughCapacity(products, containerSize) {
-  let totalProducts = 0;
-  for (let product in products) {
-    totalProducts += products[product];
-  }
-  return totalProducts <= containerSize;
+// Якщо змінна a дорівнює 10, виведіть 'Вірно', інакше виведіть 'Невірно'.
+
+let a = 10;
+if (a === 10) {
+  console.log('Вірно');
+} else {
+  console.log('Невірно');
 }
-console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
 
-console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+// Змінна num може набувати 4 значення: 1, 2, 3 або 4. Якщо вона має
+// значення '1', то у змінну result запишемо 'зима', якщо має значення
+// '2' - 'весна' і так далі.
 
-console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+let num = 2; // Змініть значення для перевірки інших випадків
+let result;
 
-console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
+switch (num) {
+  case 1:
+    result = 'зима';
+    break;
+  case 2:
+    result = 'весна';
+    break;
+  case 3:
+    result = 'літо';
+    break;
+  case 4:
+    result = 'осінь';
+    break;
+  default:
+    result = 'невідомо';
+}
+
+console.log(result);
+
+// Напишіть програму, яка отримає від користувача
+//число (кількість хвилин) і виведе у консоль
+//рядок у форматі годин і хвилин
+//70 === 01:10
+
+function formatTime(minutes) {
+  let hours = Math.floor(minutes / 60);
+  let remainingMinutes = minutes % 60;
+
+  let formattedHours = hours.toString().padStart(2, '0');
+  let formattedMinutes = remainingMinutes.toString().padStart(2, '0');
+
+  return `${formattedHours}:${formattedMinutes}`;
+}
+
+let minutesInput = 70; // Отримайте це значення від користувача
+console.log(formatTime(minutesInput));
